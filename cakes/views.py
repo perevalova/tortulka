cb864f3f72496ce11eb75adb290a750a157b58ca
Anastasia Perevalova <perev4n@gmail.com>
2020-02-04 21:53:58 +0200

    refactor views and templates

--- cakes/views.py
+++ cakes/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+
+from cakes.models import Category, Product
 
 
 class MainPageView(TemplateView):
     template_name = 'main.html'
 
 
-class CategoryView(TemplateView):
-    template_name = 'categories_list.html'
+class CategoryView(ListView):
+    template_name = 'category_list.html'
+    model = Category
 
 
-class CakeList(TemplateView):
-    template_name = 'cakes_list.html'
+class ProductList(ListView):
+    model = Product
+    template_name = 'product_list.html'
 
 
-class CakeDetail(TemplateView):
-    template_name = 'cake_detail.html'
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'product_detail.html'
+
+    def get_object(self, queryset=None):
+        obj = Product.objects.get(category__slug=self.kwargs['category'], slug=self.kwargs['product'])
+        return obj
 
 
 class ContactsView(TemplateView):
