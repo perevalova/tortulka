c5cd630824d29c4423977edcf7d0261dab3437d1
Anastasia Perevalova <perev4n@gmail.com>
2020-02-05 00:20:27 +0200

    add pagination

--- cakes/views.py
+++ cakes/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
 
 from cakes.models import Category, Product
-
+from .util import paginate
 
 class MainPageView(TemplateView):
     template_name = 'main.html'
@@ -16,6 +15,30 @@ class CategoryView(ListView):
 class ProductList(ListView):
     model = Product
     template_name = 'product_list.html'
+    paginate_by = 12
+
+    def get_queryset(self):
+        product_list = Product.objects.filter(category__slug=self.kwargs['slug'])
+
+        order_by = self.request.GET.get('order_by', '')
+        if order_by == 'newest':
+            product_list = product_list.order_by('-added', 'id')
+        elif order_by == 'oldest':
+            product_list = product_list.order_by('added', 'id')
+        elif order_by == 'abc':
+            product_list = product_list.order_by('title', 'id')
+
+        return product_list
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        product_amount = self.get_queryset().count()
+        context = paginate(self.get_queryset(), self.paginate_by, self.request,
+                           context, var_name='products')
+        context['product_amount'] = product_amount
+
+        return context
 
 
 class ProductDetail(DetailView):
