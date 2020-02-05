a140f4ed2847f4ac9d43b9e8d5ef5d88692d24fe
Anastasia Perevalova <perev4n@gmail.com>
2020-02-05 22:40:32 +0200

    add searching

--- cakes/urls.py
+++ cakes/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from cakes.views import MainPageView, ContactsView, CategoryView, ProductList, \
-    ProductDetail
+    ProductDetail, SearchView
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='home'),
     path('products/', CategoryView.as_view(), name='products'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('search/', SearchView.as_view(), name='search'),
     path('<slug:slug>/', ProductList.as_view(), name='product_list'),
     path('<slug:category>/<slug:product>', ProductDetail.as_view(), name='product_detail'),
 ]
--- cakes/views.py
+++ cakes/views.py
@@ -18,7 +18,7 @@ class ProductList(ListView):
     paginate_by = 12
 
     def get_queryset(self):
-        product_list = Product.objects.filter(category__slug=self.kwargs['slug'])
+        product_list = Product.objects.filter(category__slug=self.kwargs['slug']).prefetch_related('category', 'images')
 
         order_by = self.request.GET.get('order_by', '')
         if order_by == 'newest':
@@ -46,9 +46,37 @@ class ProductDetail(DetailView):
     template_name = 'product_detail.html'
 
     def get_object(self, queryset=None):
-        obj = Product.objects.get(category__slug=self.kwargs['category'], slug=self.kwargs['product'])
+        obj = Product.objects.prefetch_related(
+                'category', 'images').get(category__slug=self.kwargs['category'], slug=self.kwargs['product'])
         return obj
 
 
 class ContactsView(TemplateView):
     template_name = 'contacts.html'
+
+
+class SearchView(ListView):
+    template_name = 'search.html'
+    model = Product
+    context_object_name = 'products'
+    paginate_by = 10
+
+    def get_queryset(self):
+        search_product = self.request.GET.get('q', '')
+        products = Product.objects.none()
+        if search_product:
+            products = Product.objects.filter(title__icontains=search_product).prefetch_related('category', 'images')
+        # else:
+        #     products = ''
+
+        return products
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        product_amount = self.get_queryset().count()
+        context = paginate(self.get_queryset(), self.paginate_by, self.request,
+                           context, var_name='products')
+        context['product_amount'] = product_amount
+
+        return context
