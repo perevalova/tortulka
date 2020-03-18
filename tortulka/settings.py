d02067a449dee69e790d2aec58651feddd1d3379
Anastasia Perevalova <perev4n@gmail.com>
2020-03-18 23:58:00 +0200

    add Celery

--- tortulka/settings.py
+++ tortulka/settings.py
@@ -218,3 +218,10 @@ LOGGING = {
         }
     }
 }
+
+# Celery
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
